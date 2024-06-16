c23c7ea67e0613f01b850108f2d965f22b000d98
Nathaniel Tabacchi <ntabacchi@outlook.com>
2024-06-16 01:39:19 -0400

    Moved cell code to its own file.

---
+++ src/cell.py
@@ -0,0 +1,36 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, win=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        # Top left corner
+        self._x1 = None
+        self._y1 = None
+        # Top right corner
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1: int, y1: int, x2: int, y2: int):
+        # Top left corner
+        self._x1 = x1
+        self._y1 = y1
+        # Top right corner
+        self._x2 = x2
+        self._y2 = y2
+        if self.has_left_wall:
+            wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(wall, "#00FF00")
+        if self.has_right_wall:
+            wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(wall, "#00FF00")
+        if self.has_top_wall:
+            wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(wall, "#00FF00")
+        if self.has_bottom_wall:
+            wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(wall, "#00FF00")
--- src/main.py
+++ src/main.py
@@ -1,19 +1,12 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    #win.draw_line(Line(Point(50, 50), Point(50, 100)), "#00FF00")
-    #win.draw_line(Line(Point(200, 300), Point(400, 300)), "#00FF00")
     cells = []
-    x = 20
-    y = 20
     for i in range(16):
-        print(f"{i} % 4 = {i%4}")
-        if i % 4 == 0 and i != 0:
-            x = 20
-            y += 40
-        cell = Cell(x, y, x+20, y+20, win=win)
+        cell = Cell(win=win)
         if i & 1 == 1:
             cell.has_top_wall = False
         if i & 2 == 2:
@@ -23,13 +16,14 @@ def main():
         if i & 8 == 8:
             cell.has_left_wall = False
         cells.append(cell)
+    x = 20
+    y = 20
+    for i in range(16):
+        if i % 4 == 0 and i != 0:
+            x = 20
+            y += 40
+        cells[i].draw(x, y, x+20, y+20)
         x += 40
-        print(f"{i}&1: {i&1}")
-        print(f"{i}&2: {i&2}")
-        print(f"{i}&4: {i&4}")
-        print(f"{i}&8: {i&8}")
-    for cell in cells:
-        cell.draw()
     win.wait_for_close()
 
 
